ff2376f2a45cb84d629f9b0c075e246b99568163
deckerayers <ayersdecker@gmail.com>
2023-03-19 10:09:46 -0400

    Initial In

--- OpenWeatherNuGet/Call.cs
+++ OpenWeatherNuGet/Call.cs
@@ -1,7 +1,40 @@
-﻿namespace OpenWeatherNuGet;
+﻿using Newtonsoft.Json;
 
-// All the code in this file is included in all platforms.
-public class Class1
+namespace OpenWeatherNuGet;
+
+public class Call
 {
+
+    public static async Task<dynamic> OpenWeather(string ApiKey)
+    {
+
+        // Get Location from device (Latitude and Longitude) using manifest
+        var location = await Geolocation.GetLocationAsync();
+        double latitude = location.Latitude;
+        double longitude = location.Longitude;
+
+        // Key and URL Build (Imperial)
+        string key = ApiKey;
+        string url = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={key}&units=imperial";
+
+        // Build and Connect to OpenWeather API
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        // Get JSON Data
+        string content = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<dynamic>(content);
+    
+    }
+
+    public static ImageSource OpenWeatherIconSource(dynamic content)
+    {
+        // Uses the dynamic provided to return image source at specific part of content's json tree
+        return $"https://openweathermap.org/img/wn/{content.weather[0].icon.ToString()}.png";
+    }
+        
+
 }
 
